97bb3babafde810b612e87966bdab8e35b826aa5
jettcelis <junesshr@gmail.com>
2024-07-15 08:01:31 +0800

    Add temporary lib for disabling buttons when clicked (PTB)

---
+++ cdn/lib/button.js
@@ -0,0 +1,10 @@
+
+ 
+$( "#bind" ).click(function() {
+   $(this).attr("disabled", "disabled");
+   $("#unbind").removeAttr("disabled");
+});
+$( "#unbind" ).click(function() {
+     $(this).attr("disabled", "disabled");
+     $("#bind").removeAttr("disabled");
+});
